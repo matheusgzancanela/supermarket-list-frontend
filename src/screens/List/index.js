94c335a7bf8185586916cf38a08f588603a792ef
matheusgzancanela <matheusgzpessoal@gmail.com>
2023-09-23 17:30:18 -0300

    feat: create initial fetch to api

--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,3 +1,47 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+import { getList } from "../../services/request";
+import { ListCard } from "../../components";
+
 export const ListScreen = () => {
-  return <div>LISTA</div>;
+  const [loading, setLoading] = useState(true);
+  const [listData, setListData] = useState([]);
+
+  const loadListItems = async () => {
+    setLoading(true);
+    const result = await getList();
+    console.log({ result });
+    setListData(result);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadListItems();
+  }, []);
+
+  return (
+    <div className="list-screen-container">
+      <div className="list-screen-content-container">
+        <div className="list-creen-header">
+          <img
+            className="logo-image"
+            src="/images/logo.png"
+            alt="supermaerket-list-logo"
+          />
+          <h1>Lista Supermercado</h1>
+        </div>
+        <div className="list-screen-list-container">
+          {loading && <h3>Carregando...</h3>}
+          {!loading && listData?.length > 0 ? (
+            listData.map((item) => <ListCard key={item._id} item={item} />)
+          ) : (
+            <h3>
+              Sua lista ésta vazia, adicione um novo item clicando no botão de
+              "Adicionar"
+            </h3>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
